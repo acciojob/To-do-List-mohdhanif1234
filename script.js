c22b94c57cb7ef70cd129b974a2b2ea9988c15fe
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2023-02-24 20:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+let addTodoBtn = document.getElementById("addTodoBtn");
+addTodoBtn.addEventListener("click",()=>{
+	let newTodoInput = document.getElementById("newTodoInput").value;
+	if(newTodoInput !== ""){
+		document.getElementById("list").innerHTML = newTodoInput;
+	}
+})
